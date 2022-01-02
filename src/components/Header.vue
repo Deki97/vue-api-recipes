66f695d7ce5e46f0a95602ac8df4ff6628ee6cb9
Deki97 <dejan97@libero.it>
2022-01-02 16:07:18 +0100

    Header done

---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+    <header>
+        <div class="logo">
+            <img src="../assets/img/cooking-mama-logo.png" alt="Cooking Mama Logo">
+        </div>
+
+        <div class="title">
+            <h1>Recipes API</h1>
+        </div>
+    </header>
+</template>
+
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+
+header {
+    background-color: $primary-color;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 50px;
+    .logo {
+        width: 180px;
+    }
+
+    .title {
+        color: $secondary-color;
+
+        h1 {
+            font-size: 50px;
+        }
+    }
+}
+</style>
---
+++ src/components/Recipe.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="recipe">
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Recipe'
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
